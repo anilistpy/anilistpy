34b05b7fe5dec82e1965a912a7333720d4c56375
Aaditya Aryal <aryalaadi123@gmail.com>
2020-11-28 17:48:46 +0545

    Anime search by id

---
+++ anilistpy/__init__.py
@@ -0,0 +1,1 @@
+from anilistpy.anime import Anime
---
+++ anilistpy/anime.py
@@ -0,0 +1,60 @@
+import requests
+import json
+class Anime:
+    def __init__(self, sQ):
+        query = '''
+        query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+            Page (page: $page, perPage: $perPage) {
+                media (id: $id, search: $search, type: ANIME) {
+                    id
+                    title {
+                        romaji
+                    }
+                    episodes
+                    description
+                    duration
+                    genres
+                    averageScore
+                    tags{
+                        name
+                    }
+                    studios {
+                        nodes{
+                            name
+                        }
+                    }
+                }
+            }
+        }
+        '''
+
+        variables = {
+            'id': sQ,
+            'page': 1,
+            'perPage': 1,
+            'MediaType': "ANIME"
+        }
+
+        url = 'https://graphql.anilist.co'
+
+        response = requests.post(url, json={'query': query, 'variables': variables})
+        raw = json.loads(response.text)
+        self.media = raw["data"]["Page"]["media"]
+           
+
+    def title(self): 
+        return self.media[0]["title"]["romaji"]
+    def episodes(self):
+        return self.media[0]["episodes"]
+    def description(self):
+        return self.media[0]["description"]
+    def duration(self):
+        return self.media[0]["duration"]
+    def genres(self):
+        return self.media[0]["genres"]
+    def averageScore(self):
+        return self.media[0]["averageScore"]    
+    def tags(self):
+        return self.media[0]["tags"]
+    def studios(self):
+        return self.media[0]["studios"]["nodes"]
