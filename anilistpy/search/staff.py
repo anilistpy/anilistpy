f75a5cf78d443e82b04c837e1df84030c917b80f
0kalekale <kalekale.anon@gmail.com>
2020-12-20 23:06:57 +0545

    added template for staff modules

---
+++ anilistpy/search/staff.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+class searchStaff:
+    def __init__(self, sQ):
+        pass
+    
+    def _test(self):
+        return True
---
+++ anilistpy/staff.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+class Staff:
+    def __init__(self, sQ):
+        pass
+    
+    def _test(self):
+        return True
