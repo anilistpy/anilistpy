8aafc60e777aa18ac8ce00ebf0824735a0dfa92d
0kalekale <kalekale.anon@gmail.com>
2020-12-21 22:33:36 +0545

    added query for Staff()

--- anilistpy/staff.py
+++ anilistpy/staff.py
@@ -3,7 +3,51 @@ import json
 
 class Staff:
     def __init__(self, sQ):
-        pass
-    
+        self.id = sQ
+        query='''
+query ($id: Int) {
+  Staff(id: $id, sort: FAVOURITES_DESC) {
+    id
+    name {
+      first
+      last
+      full
+      native
+    }
+    language
+    image {
+      large
+      medium
+    }
+    description
+    isFavourite
+    siteUrl
+    staffMedia {
+      edges {
+        node {
+          id
+          title {
+            romaji
+            english
+            native
+          }
+        }
+      }
+    }
+  }
+}
+
+        '''
+        variables = {
+            'id': sQ
+        }  
+
+        url = 'https://graphql.anilist.co'
+
+        response = requests.post(url, json={'query': query, 'variables': variables})
+        self.raw = json.loads(response.text)
+        self.media = self.raw["data"]["Staff"]
+
     def _test(self):
-        return True
+        return self.media
+        
