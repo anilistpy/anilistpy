cc3de5faa96c21cd78ea18f02bfc061bf0739d3d
0kalekale <kalekale.anon@gmail.com>
2020-12-21 22:33:25 +0545

    added query for Studio()

--- anilistpy/studio.py
+++ anilistpy/studio.py
@@ -3,7 +3,35 @@ import json
 
 class Studio:
     def __init__(self, sQ):
-        pass
-    
+        self.id = sQ
+        query='''
+query ($id: Int) {
+  Studio(id: $id) {
+    id
+    name
+    media{
+      nodes{
+        title{
+          romaji
+        }
+        id
+        
+      }
+    }
+  }
+}
+
+        '''
+        variables = {
+            'id': sQ
+        }  
+
+        url = 'https://graphql.anilist.co'
+
+        response = requests.post(url, json={'query': query, 'variables': variables})
+        self.raw = json.loads(response.text)
+        self.media = self.raw["data"]["Studio"]
+
     def _test(self):
-        return True
+        return self.media
+        
