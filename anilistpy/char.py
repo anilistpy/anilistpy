01a30882f9db7840c90f2f6c6da9a523343e2e00
0kalekale <kalekale.anon@gmail.com>
2020-12-21 22:33:12 +0545

    added query for Character()

--- anilistpy/char.py
+++ anilistpy/char.py
@@ -3,7 +3,51 @@ import json
 
 class Character:
     def __init__(self, sQ):
-        pass
-    
+        self.id = sQ
+        query='''
+query ($id: Int) {
+  Character(id: $id, sort: FAVOURITES_DESC) {
+    id
+    name {
+      first
+      last
+      full
+      native
+    }
+    image {
+      large
+      medium
+    }
+    description
+    isFavourite
+    siteUrl
+    media {
+      edges {
+        node{
+          title {
+            romaji
+            english
+            native
+            userPreferred
+          }
+          id
+        }
+      }
+    }
+    favourites
+  }
+}
+        '''
+        variables = {
+            'id': sQ
+        }  
+
+        url = 'https://graphql.anilist.co'
+
+        response = requests.post(url, json={'query': query, 'variables': variables})
+        self.raw = json.loads(response.text)
+        self.media = self.raw["data"]["Character"]
+
     def _test(self):
-        return True
+        return self.media
+        
